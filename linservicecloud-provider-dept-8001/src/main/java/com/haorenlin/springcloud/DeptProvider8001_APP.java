599af3172743faf6555384753be87daee2940d61
yanjunlin <yanjunlin@midaigroup.com>
2018-07-19 23:50:08 +0800

    将服务注册进eureka

--- linservicecloud-provider-dept-8001/src/main/java/com/haorenlin/springcloud/DeptProvider8001_APP.java
+++ linservicecloud-provider-dept-8001/src/main/java/com/haorenlin/springcloud/DeptProvider8001_APP.java
@@ -1,9 +1,11 @@
 package com.haorenlin.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_APP {
 	
 	public static void main(String[] args) throws Exception {
